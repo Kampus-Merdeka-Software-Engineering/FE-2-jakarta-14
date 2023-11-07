c27eec4a32c4f6955110229c41415f36087bbc46
Aditya Abdi Susanto <141890742+Adtyabdi@users.noreply.github.com>
2023-11-07 13:29:45 +0700

    update popup login and register

--- js/home.js
+++ js/home.js
@@ -6,3 +6,23 @@ console.log(navbarnav);
 document.querySelector("#hamburger-menu").addEventListener("click", () => {
   navbarnav.classList.toggle("active");
 });
+
+// login
+const formOpenBtn = document.querySelector("#form-open"),
+  page = document.querySelector(".page"),
+  formContainer = document.querySelector(".form-container"),
+  closeBtn = document.querySelector(".close"),
+  signupBtn = document.querySelector("#signup"),
+  loginBtn = document.querySelector("#login");
+
+formOpenBtn.addEventListener("click", () => page.classList.add("show"));
+closeBtn.addEventListener("click", () => page.classList.remove("show"));
+
+signupBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  formContainer.classList.add("active");
+});
+loginBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  formContainer.classList.remove("active");
+});
