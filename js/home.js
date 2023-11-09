5cb4c886ed82a380950cf6c6dcf6d5e2aa6b5c45
Aditya Abdi Susanto <141890742+Adtyabdi@users.noreply.github.com>
2023-11-09 22:14:58 +0700

    popup

--- js/home.js
+++ js/home.js
@@ -26,3 +26,18 @@ loginBtn.addEventListener("click", (e) => {
   e.preventDefault();
   formContainer.classList.remove("active");
 });
+
+// feedback carousel
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
