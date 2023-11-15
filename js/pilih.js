e3a8c49326967f78a5acf562e5ee8a593f5c81b9
Aditya Abdi Susanto <141890742+Adtyabdi@users.noreply.github.com>
2023-11-16 00:08:06 +0700

    page dua down

--- js/pilih.js
+++ js/pilih.js
@@ -6,3 +6,47 @@ console.log(navbarnav);
 document.querySelector("#hamburger-menu").addEventListener("click", () => {
   navbarnav.classList.toggle("active");
 });
+
+// login
+const formOpenBtn = document.querySelector("#open"),
+  register = document.querySelector(".page-login"),
+  formContainer = document.querySelector(".form-container"),
+  closeBtn = document.querySelector(".close"),
+  signupBtn = document.querySelector("#signup"),
+  loginBtn = document.querySelector("#login");
+
+formOpenBtn.addEventListener("click", () => register.classList.add("show"));
+closeBtn.addEventListener("click", () => register.classList.remove("show"));
+
+signupBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  formContainer.classList.add("active");
+});
+loginBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  formContainer.classList.remove("active");
+});
+
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  freeMode: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
