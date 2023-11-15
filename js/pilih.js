6d25ec0900a5be422c8c96d192c4054c27d1396f
Aditya Abdi Susanto <141890742+Adtyabdi@users.noreply.github.com>
2023-11-16 00:21:51 +0700

    page lokasi selesai

--- js/pilih.js
+++ js/pilih.js
@@ -27,15 +27,6 @@ loginBtn.addEventListener("click", (e) => {
   formContainer.classList.remove("active");
 });
 
-var swiper = new Swiper(".mySwiper", {
-  slidesPerView: 3,
-  spaceBetween: 30,
-  freeMode: true,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-});
 
 var swiper = new Swiper(".mySwiper", {
   slidesPerView: 3,
