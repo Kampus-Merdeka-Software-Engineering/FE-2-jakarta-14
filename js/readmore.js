08a167b15cd403dfedb6a55fa6649cb13c3a1b4a
Aditya Abdi Susanto <141890742+Adtyabdi@users.noreply.github.com>
2023-11-19 15:36:02 +0700

    page read more

--- js/readmore.js
+++ js/readmore.js
@@ -8,15 +8,15 @@ document.querySelector("#hamburger-menu").addEventListener("click", () => {
 });
 
 // login
-const formOpenBtn = document.querySelector("#form-open"),
-  page = document.querySelector(".page"),
+const formOpenBtn = document.querySelector("#open"),
+  register = document.querySelector(".page-login"),
   formContainer = document.querySelector(".form-container"),
   closeBtn = document.querySelector(".close"),
   signupBtn = document.querySelector("#signup"),
   loginBtn = document.querySelector("#login");
 
-formOpenBtn.addEventListener("click", () => page.classList.add("show"));
-closeBtn.addEventListener("click", () => page.classList.remove("show"));
+formOpenBtn.addEventListener("click", () => register.classList.add("show"));
+closeBtn.addEventListener("click", () => register.classList.remove("show"));
 
 signupBtn.addEventListener("click", (e) => {
   e.preventDefault();
@@ -25,19 +25,4 @@ signupBtn.addEventListener("click", (e) => {
 loginBtn.addEventListener("click", (e) => {
   e.preventDefault();
   formContainer.classList.remove("active");
-});
-
-// feedback carousel
-var swiper = new Swiper(".mySwiper", {
-  slidesPerView: 1,
-  grabCursor: true,
-  loop: true,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
 });
